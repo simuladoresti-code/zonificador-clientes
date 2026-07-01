f88bbd7caed434c7af6f9614761bd1e9ced05fc4
simuladoresti-code <simuladoresti@gmail.com>
2026-07-01 09:43:14 -0500

    Refactor app.py to add file upload and download routes

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,52 @@
+from flask import Flask, jsonify, request, send_file
+from processor import procesar_datos
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({"mensaje": "Servidor activo"})
+
+
+@app.route("/subir", methods=["POST"])
+def subir():
+
+    clientes = request.files["clientes"]
+    poligonos = request.files["poligonos"]
+
+    clientes.save("clientes.kmz")
+    poligonos.save("poligonos.kmz")
+
+    return jsonify({
+        "mensaje": "Archivos subidos correctamente"
+    })
+
+
 @app.route("/actualizar")
 def actualizar():
 
-    archivo, clientes, poligonos = procesar_datos(
+    archivo, total_clientes, total_poligonos = procesar_datos(
         "clientes.kmz",
         "poligonos.kmz"
     )
 
     return jsonify({
         "estado": "ok",
-        "clientes": clientes,
-        "poligonos": poligonos
+        "clientes": total_clientes,
+        "poligonos": total_poligonos
     })
+
+
+@app.route("/descargar")
+def descargar():
+    return send_file(
+        "clientes_actualizados.xlsx",
+        as_attachment=True
+    )
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
