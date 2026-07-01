b677c0c567a14a76c400df568696bc8ef5cbd239
simuladoresti-code <simuladoresti@gmail.com>
2026-06-30 22:09:11 -0500

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,18 @@
-function actualizar() {
-    window.open("https://TU-APP-RENDER.onrender.com/actualizar", "_blank");
+const API_URL = "https://zonificador-clientes-7.onrender.com";
+
+function actualizarDatos() {
+    const estado = document.getElementById("estado");
+
+    estado.innerHTML = "Procesando información...";
+
+    window.open(API_URL + "/actualizar", "_blank");
+
+    document.getElementById("fecha").innerText =
+        new Date().toLocaleString();
+
+    estado.innerHTML = "Actualización completada.";
+}
+
+function descargarExcel() {
+    window.open(API_URL + "/actualizar", "_blank");
 }
